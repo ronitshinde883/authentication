41dc1919d1cb62dba13886a5565174431b89bd71
Ronit Shinde <shinderonit4@gmail.com>
2026-03-23 19:17:17 +0530

    added serializers

--- authapp/serializers.py
+++ authapp/serializers.py
@@ -1,11 +1,31 @@
 from rest_framework import serializers
-from .models import UserProfile
+from .models import Userprofile
 from django.contrib.auth.models import User
 
 class Registerserializer(serializers.ModelSerializer):
-    phone=serializers.CharField(write_only=True)
+    username = serializers.CharField(write_only=True)
+    email = serializers.EmailField(write_only=True)
+    password = serializers.CharField(write_only=True)
     
     class Meta:
-        model=User
-        fields=['username', 'email', 'password', 'phone']
+        model = Userprofile
+        fields =['username', 'email', 'password', 'phone']
         
+    def create(self,validated_data):
+        username = validated_data.pop('username')
+        email = validated_data.pop('email')
+        password = validated_data.pop('password')
+        
+        user = User.objects.create_user(
+            username=username,
+            email=email,
+            password=password
+        )
+
+        # 🔥 create profile
+        profile = Userprofile.objects.create(
+            user=user,
+            **validated_data
+        )
+
+        return profile
