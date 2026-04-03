0beddf93672d4f87466582fc8f650cd67c9ffa3e
Ronit Shinde <shinderonit4@gmail.com>
2026-04-03 13:15:27 +0530

    Logout api integrated

--- authapp/urls.py
+++ authapp/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path,include
 from . import views
-from .views.auth_views import RegisterAPI,LoginAPI
+from .views.auth_views import RegisterAPI,LoginAPI,LogoutAPI
 
 urlpatterns = [
     path("api/register/", RegisterAPI.as_view(), name="api_register"),
     path("api/login/", LoginAPI.as_view(), name="api_login"),
+    path("api/logout/", LogoutAPI.as_view(), name="api_logout")
 ]
--- authapp/views/auth_views.py
+++ authapp/views/auth_views.py
@@ -4,8 +4,9 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth.tokens import default_token_generator
-from django.contrib.auth import login as auth_login, authenticate
+from django.contrib.auth import login as auth_login, authenticate,logout
 from django.contrib.auth.models import User
+
 
 #will be updating to jwt based authentication later
 class RegisterAPI(APIView):
@@ -41,4 +42,12 @@ class LoginAPI(APIView):
             {"error": "Invalid email or password"},
             status=status.HTTP_401_UNAUTHORIZED
         )
+class LogoutAPI(APIView):
+    permission_classes=[IsAuthenticated]
     
+    def post(self,request):
+        logout(request)
+        return Response(
+            {"message": "Logged out successfully"},
+            status=status.HTTP_200_OK
+        )
