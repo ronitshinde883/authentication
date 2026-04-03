6cf31b55a8e868d0f74983a1f880c766999f4b27
Ronit Shinde <shinderonit4@gmail.com>
2026-04-03 13:13:08 +0530

    added login api

--- authapp/urls.py
+++ authapp/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path,include
 from . import views
-from .views.auth_views import RegisterAPI
+from .views.auth_views import RegisterAPI,LoginAPI
 
 urlpatterns = [
     path("api/register/", RegisterAPI.as_view(), name="api_register"),
+    path("api/login/", LoginAPI.as_view(), name="api_login"),
 ]
--- authapp/views/auth_views.py
+++ authapp/views/auth_views.py
@@ -4,8 +4,8 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth.tokens import default_token_generator
-
-
+from django.contrib.auth import login as auth_login, authenticate
+from django.contrib.auth.models import User
 
 #will be updating to jwt based authentication later
 class RegisterAPI(APIView):
@@ -18,4 +18,27 @@ class RegisterAPI(APIView):
                 status=status.HTTP_201_CREATED
             )
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class LoginAPI(APIView):
+    def post(self,request):
+        email=request.data.get("email")
+        password=request.data.get("password")
+        
+        try:
+            user_obj=User.objects.get(email=email)
+        except User.DoesNotExist:
+            return Response({
+                "error":"Invalid Email Or Password"
+            },status=status.HTTP_401_UNAUTHORIZED)
+        user=authenticate(username=user_obj.username,password=password)
+        
+        if user:
+            return Response({
+                "message":"Login succesful"
+            },status=status.HTTP_200_OK)
+            
+        return Response(
+            {"error": "Invalid email or password"},
+            status=status.HTTP_401_UNAUTHORIZED
+        )
     
