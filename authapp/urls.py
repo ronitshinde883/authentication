43a8534babcd7ae60476563b013be5494b208ea7
Ronit Shinde <shinderonit4@gmail.com>
2026-04-04 23:48:10 +0530

    changes

--- authapp/urls.py
+++ authapp/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path,include
 from . import views
-from .views.auth_views import RegisterAPI,LoginAPI,LogoutAPI
+from .views.auth_views import RegisterAPI,LoginAPI,LogoutAPI,ForgotPasswordAPI
 
 urlpatterns = [
     path("api/register/", RegisterAPI.as_view(), name="api_register"),
     path("api/login/", LoginAPI.as_view(), name="api_login"),
-    path("api/logout/", LogoutAPI.as_view(), name="api_logout")
+    path("api/logout/", LogoutAPI.as_view(), name="api_logout"),
+    path("api/forgot-password/",ForgotPasswordAPI.as_view(), name="api_forgot_password"),
 ]
